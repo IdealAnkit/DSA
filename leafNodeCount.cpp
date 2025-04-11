23d88378d810942fa4892bc096d41c977eebe48f
IdealAnkit <mrankitkumar1530@gmail.com>
2025-04-11 16:17:00 +0530

    LeafNode

---
+++ leafNodeCount.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+using namespace std;
+int ln=0;
+struct BinaryTree
+{
+    int data;
+    BinaryTree *left;
+    BinaryTree *right;
+};
+
+BinaryTree *NewNode, *root, *temp;
+
+/*
+
+    {
+                 n1
+                /  \
+               n2   n3
+              / \
+             n4  n5
+    }
+
+*/
+
+void Traversing(BinaryTree *temp)
+{
+    if (temp == nullptr)
+    {
+        return;
+    }
+    if (temp->left==nullptr && temp->right==nullptr)
+    {
+        ln++;
+    }
+    
+    Traversing(temp->left);
+    Traversing(temp->right);
+}
+
+void InOrder(BinaryTree *temp)
+{
+    if (temp == nullptr)
+    {
+        return;
+    }
+
+    InOrder(temp->left);
+    cout << temp->data << " ";
+    InOrder(temp->right);
+}
+
+void PreOrder(BinaryTree *temp)
+{
+    if (temp == nullptr)
+    {
+        return;
+    }
+
+    cout << temp->data << " ";
+    PreOrder(temp->left);
+    PreOrder(temp->right);
+}
+
+void PostOrder(BinaryTree *temp)
+{
+    if (temp == nullptr)
+    {
+        return;
+    }
+
+    PostOrder(temp->left);
+    PostOrder(temp->right);
+    cout << temp->data << " ";
+}
+
+BinaryTree *NewNodeCreation(int val)
+{
+    NewNode= new BinaryTree();
+    NewNode->data=val;
+    NewNode->left=nullptr;
+    NewNode->right=nullptr;
+    return NewNode;
+}
+
+int main()
+{
+    root=NewNodeCreation(5);
+    root->left=NewNodeCreation(6);
+    root->right=NewNodeCreation(7);
+    root->left->left=NewNodeCreation(8);
+    root->left->right=NewNodeCreation(9);
+    root->left->right->left=NewNodeCreation(12);
+    root->left->right->right=NewNodeCreation(62);
+    root->right->right=NewNodeCreation(92);
+
+    cout << "Preorder :- ";
+    PreOrder(root);
+    cout << endl;
+
+    cout << "Inorder :- ";
+    InOrder(root);
+    cout << endl;
+
+    cout << "Postorder :- ";
+    PostOrder(root);
+    cout << endl;
+
+    cout << "Total Leaf Node :- ";
+    Traversing(root);
+    cout<<ln;
+    cout << endl;
+
+    return 0;
+}
