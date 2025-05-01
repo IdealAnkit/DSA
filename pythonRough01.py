95685511f8ce858a898f4485393f32e314468d16
IdealAnkit <mrankitkumar1530@gmail.com>
2025-05-01 11:53:48 +0530

    correcting 04 py

--- pythonRough01.py
+++ pythonRough01.py
@@ -1,2 +1,9 @@
-print(float("1, 3"))
+matrix = [[x for x in range(3)] for y in range(3)]
+
+count = 0
+for row in matrix:
+    for element in row:
+        if element % 2 != 0:
+            count += 1
+print(count)
 
