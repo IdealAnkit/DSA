d7c35b99f3e19a91e5ac3369af88e0f8528800ed
IdealAnkit <mrankitkumar1530@gmail.com>
2025-05-08 15:58:06 +0530

    python star pattern 01 5 types

--- pythonRough01.py
+++ pythonRough01.py
@@ -1,9 +1,30 @@
-matrix = [[x for x in range(3)] for y in range(3)]
+n = 5  # You can change the value of n to increase/decrease pattern size
 
-count = 0
-for row in matrix:
-    for element in row:
-        if element % 2 != 0:
-            count += 1
-print(count)
+# Pattern 1: Left-aligned right-angle triangle
+print("Pattern 1: Left-aligned right-angle triangle")
+for i in range(1, n + 1):
+    print("*" * i)
 
+print("\nPattern 2: Right-aligned right-angle triangle")
+# Pattern 2: Right-aligned right-angle triangle
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * i)
+
+print("\nPattern 3: Pyramid")
+# Pattern 3: Pyramid
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+
+print("\nPattern 4: Inverted pyramid")
+# Pattern 4: Inverted pyramid
+for i in range(n, 0, -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+
+print("\nPattern 5: Diamond")
+# Pattern 5: Diamond
+# Top half
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+# Bottom half
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
