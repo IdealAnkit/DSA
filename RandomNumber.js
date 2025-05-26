d6f14540b07313db9f4f0f519dd88435cea7c478
Ankit Kumar <mrankitkumar1530@gmail.com>
2025-05-26 22:35:00 +0530

    Random Number 1 to 100

---
+++ RandomNumber.js
@@ -0,0 +1,8 @@
+// Generate a random number between 1 and 100 (inclusive)
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+// Example usage
+const randomNum = getRandomNumber();
+console.log(randomNum);
