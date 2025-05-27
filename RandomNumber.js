6ba052e725c135b29620d7c7162b16f179ce8b93
Ankit Kumar <mrankitkumar1530@gmail.com>
2025-05-27 17:33:06 +0530

    Random num upto 30

--- RandomNumber.js
+++ RandomNumber.js
@@ -1,8 +1,7 @@
 // Generate a random number between 1 and 100 (inclusive)
 function getRandomNumber() {
-    return Math.floor(Math.random() * 100) + 1;
+    return Math.floor(Math.random() * 30) + 1;
 }
 
-// Example usage
 const randomNum = getRandomNumber();
 console.log(randomNum);
